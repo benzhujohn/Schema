6d79452520cfeb245aea31ea739f6eb186097a3e
benzhujohn <83063761+benzhujohn@users.noreply.github.com>
2022-11-24 17:05:56 +0800

    Create product-index.js

---
+++ Schema/product-index.js
@@ -0,0 +1,71 @@
+
+        <!---------product-->
+
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org/",
+                "@type": "Product",
+                "name": "25.6V 2.5KWH ES-BOX17 LiFePO4 Wall-Mounted Home Energy Storage Battery",
+                "sku": "Genxigreen",
+                
+                "gtin12": "ES-BOX17",
+                "url" : "https://www.genixenergy.com/power-storage-wall/25.6v-2.5kwh-es-box17-lifepo4-wall-mounted-home-energy-storage-battery.html",
+                "description": "",
+                "image": "https://www.genixenergy.com/data/upload/ueditor/20221123/637deaec7b9bd.jpg",
+                "aggregateRating": {
+                    "@type": "AggregateRating",
+                    "ratingValue": "0",
+                    "reviewCount": "0"
+                },
+                "review": [
+                ],
+                "offers": {
+                    "@type": "Offer",
+                    "priceCurrency": "USD",
+                    "price": " ",
+                    "itemCondition" : "https://schema.org/NewCondition",
+                    "availability" : "https://schema.org/InStock",
+                    "url" : "https://www.genixenergy.com/power-storage-wall/25.6v-2.5kwh-es-box17-lifepo4-wall-mounted-home-energy-storage-battery.html",
+                    "priceValidUntil": "2024-11-24"
+                }
+            }
+        </script>
+
+<!---------product--2>
+
+        <script type="application/ld+json">
+            {
+              "@context": "http://schema.org",
+              "@type": "Product",
+              "offers": {
+                "@type": "Offer",
+                "availability":"https://schema.org/InStock",
+                "price": "1299.0",
+                "priceCurrency": "AUD",
+                "priceValidUntil": "2022-12-04",
+                "url": "https://itechworld.com.au/products/itech200-12v-200ah-lithium-ion-battery-lifepo4-deep-cycle-recycle-camping-rv-solar"
+              },
+              "brand": "iTechworld",
+              "sku": "200",
+              "name": "12v iTECH200 200Ah Lithium Battery - LiFePO4 Deep Cycle Camping RV Solar",
+              "description": "iTECH200 200Ah 12V Lithium-ion Battery.  In Stock.\n\nWe are introducing our latest lithium deep cycle battery, the iTECH200. A massive 200 Amps of high-performance usable power coupled with the Redback™️ Lithium Operating System which ensures iTechworld's lithium battery range will outperform and outlast all others on the market. Designed specifically for use with inverters up to and including 3000W.\n\nBMS\/OPERATING SYSTEM\n100% of the iTECH200's capacity is usable. The internal BMS allows 250A continuous discharge; other sub-standard lithium batteries have either no continuous discharge rating or a low continuous discharge rating of 50A - 100A. The iTECH200's internal BMS controls cell balancing, over and under-voltage protection, short circuit protection, thermal protection, and has over-discharge protection (safe mode).\n\nCAPACITY\nThe iTECH200 is the equivalent to 400Ah in lead-acid batteries (based on the standard depth of discharge of the iTECH200 (100%) and AGM battery (50%).\n\nWEIGHT\nThe iTECH200 weighs just 27kg. The equivalent lead-acid battery weighs 120+kg.\n\nUSE MOST CHARGERS\nThe iTECH200 deep cycle battery will charge up to 5x faster than a traditional lead-acid battery. The iTECH200 can charge from just about all existing chargers with no extensive modifications required to existing systems.\n\nLIFESPAN\nThe iTECH200 can cycle up to 5000 times at 50% depth of discharge. AGM battery equivalents can cycle up to 500 times at a 50% depth of discharge. iTECH200 lithium battery systems have little to no self-discharge rate(≤2%). The battery pack can sit for long periods and will not go flat.\n\nAUSTRALIAN DESIGNED AND DEVELOPED\nOur lithium batteries are designed and developed in Australia for use with caravans, camper trailers, motor homes, 4WD, boats, yachts, trucks and everything in-between. The iTECH200 will operate flawlessly in extreme heat and will handle heavy vibrations usually associated with corrugated roads.\n\nFEATURES\n\nHeavy duty case\nCan be connected in parallel (up to 4) \nCan be connected in series (up to 4)\nHigh discharge current max 500A (5sec)\nHeavy-duty removable M8 Bolt Battery terminals\nMaintenance-free\nLithium-ion LiFePO4 \nLifting handles fitted to ensure a secure hold when moving\nInternal battery management system\nRedback™️ Lithium Operating System installed as standard.\nBuilt-in over-discharge protection.\n\nSPECIFICATIONS\n\n\n\nCharging Voltage:\n14.4 - 14.7v\n\n\nStandard discharge current:\n250A\n\n\nMax discharge current:\n500A (5 secs)\n\n\nStandard charge current:\n1A - 100A\n\n\nMaximum charge current:\n200A \n\n\nOutput connector:\nM8 Stud\n\n\nWaterproof:\nIP65\n\n\nDimensions (LxWxH):\n525x240x220mm\n\n\nHeight:\n235mm including terminals\n\n\nOptimal charge temp range:\n0°c to + 45°c\n\n\nOptimal discharge temp range:\n-20°c to + 60°c\n\n\nOptimal storage temp:\n-20°c to + 60°c\n\n\nTerminals:\nM8 Studs\n\n\nWeight:\n25.4Kg\n\n\n\n\nPACKAGE CONTENTS\n\n1x Lithium Single Deep Cycle Battery iTECH200\nPre-installed Redback™️ Lithium Operating System\n\niTECH200 discharge curve and voltage chart HEREIf your iTechworld battery enters safe mode, please follow the instructions HERE\n\nThe statutory warranty is 36 months from the date of purchase.\nStorage deep cycle battery only. It is not a cranking battery.\n",
+              "category": "",
+              "url": "https://itechworld.com.au/products/itech200-12v-200ah-lithium-ion-battery-lifepo4-deep-cycle-recycle-camping-rv-solar",
+                "mpn": "28.0kg",
+                "productId": "28.0kg","image": {
+                "@type": "ImageObject",
+                "url": "https://cdn.shopify.com/s/files/1/0587/1749/products/dropinreplacementlithiumbattery_8e391b67-58bc-495a-a783-bb931d9d5d9b_1024x1024.jpg?v=1624969374",
+                "image": "https://cdn.shopify.com/s/files/1/0587/1749/products/dropinreplacementlithiumbattery_8e391b67-58bc-495a-a783-bb931d9d5d9b_1024x1024.jpg?v=1624969374",
+                "name": "12v iTECH200 200Ah Lithium Battery - LiFePO4 Deep Cycle Camping RV Solar",
+                "width": 1024,
+                "height": 1024
+              }
+              
+              ,"aggregateRating": {
+                  "@type": "AggregateRating",
+                  "ratingValue": "4.9",
+                  "reviewCount": "89"
+              }
+              
+            }
+          </script>
