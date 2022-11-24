7d7693e8452357c94b79ee53e2fd94803889cbda
benzhujohn <83063761+benzhujohn@users.noreply.github.com>
2022-11-24 17:54:37 +0800

    Create Blog-index.js

---
+++ Schema/Blog-index.js
@@ -0,0 +1,55 @@
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org/",
+                "@type": "BlogPosting",
+                "@id": "https://www.genixenergy.com/battery-knowledge/how-long-will-a-100ah-battery-last.html",
+                "mainEntityOfPage": "https://www.genixenergy.com/battery-knowledge/how-long-will-a-100ah-battery-last.html",
+                "headline": "how long will a 100ah battery last",
+                "name": "how long will a 100ah battery last",
+                "description": "how long will a 100ah battery last?12v 100ah battery can be used to run your 300w computer for 4 hours",
+                "datePublished": "2022-11-7",
+                "dateModified": "2022-11-7",
+                "author": {
+                    "@type": "Person",
+                    "@id": "https://www.genixenergy.com/",
+                    "name": "Genxigreen",
+                    "url": "https://www.genixenergy.com/battery-knowledge/",
+                    "image": {
+                        "@type": "ImageObject",
+                        "@id": "https://www.genixenergy.com/themes/simplebootx/picbanners/logo.png",
+                        "url": "https://www.genixenergy.com/themes/simplebootx/picbanners/logo.png",
+                        "height": "96",
+                        "width": "96"
+                    }
+                },
+                "publisher": {
+                    "@type": "Organization",
+                    "@id": "https://www.genixenergy.com/",
+                    "name": "Shenzhen GenixGreen Technology Co., Ltd.",
+                    "logo": {
+                        "@type": "ImageObject",
+                        "@id": "https://www.genixenergy.com/themes/simplebootx/picbanners/logo.png",
+                        "url": "https://www.genixenergy.com/themes/simplebootx/picbanners/logo.png",
+                        "width": "600",
+                        "height": "60"
+                    }
+                },
+                "image": {
+                    "@type": "ImageObject",
+                    "@id": "https://www.genixenergy.com/data/upload/ueditor/20221124/637edaa7ac588.jpg",
+                    "url": "https://www.genixenergy.com/data/upload/ueditor/20221124/637edaa7ac588.jpg",
+                    "height": "362",
+                    "width": "388"
+                },
+                "url": "https://www.genixenergy.com/battery-knowledge/how-long-will-a-100ah-battery-last.html",
+                "isPartOf": {
+                    "@type" : "Blog",
+                     "@id": "https://www.genixenergy.com/battery-knowledge/",
+                     "name": "Battery Knowledge",
+                     "publisher": {
+                         "@type": "Organization",
+                         "@id": "https://www.genixenergy.com/",
+                         "name": "Shenzhen GenixGreen Technology Co., Ltd."
+                     }
+                 }}
+                </script>
